e3ad7c952e325e4aa075e7323fcca97a2c468187
Dinesh Thakur <dineshthakur.dev@gmail.com>
2022-07-16 18:05:11 +0530

    compressing bst order and sparse matrix

--- marrix/sparse_matrix.c
+++ marrix/sparse_matrix.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
 int main()
 {
-    int rows, cols, zero = 0;
-    printf("Enter rows of matrix: ");
+    int rows, cols, zeros = 0;
+    printf("Enter number of rows: ");
     scanf("%d", &rows);
-    printf("Enter columns of matrix: ");
+    printf("Enter number of columns: ");
     scanf("%d", &cols);
     int matrix[rows][cols];
+
     for (int i = 0; i < rows; i++)
     {
-        printf("Enter elements of %d' st/nd row: ", i + 1);
+        printf("Enter elements of row %d:\n", i + 1);
         for (int j = 0; j < cols; j++)
-            scanf("%d", &matrix[i][j]);
+        {   scanf("%d", &matrix[i][j]);
+            if (matrix[i][j] == 0)
+                zeros++;
+        }
     }
     printf("\n");
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            if (matrix[i][j] == 0)
-                zero++;
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    if (zero > (7 / 10) * (rows * cols))// if more than 70% of elements are zero
+
+    if (zeros > ((rows * cols)/2)) // if more than 50% of elements are zero
     { 
         printf("Matrix is sparse\n");
         printf("row \t col \t value\n");
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++)
                 if (matrix[i][j] != 0)
-                {
                     printf("%d\t %d\t %d\n", i, j, matrix[i][j]);
-                }
     }
     else
-        printf("Matrix is not sparse\n");
+        printf("Matrix is not sparse\n\n");
     
     return 0;
 }
