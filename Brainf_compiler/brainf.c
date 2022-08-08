6e104ab18c3b45efa5df518589ec640f16123f90
Dinesh Thakur <dineshthakur.dev@gmail.com>
2022-08-08 23:46:55 +0530

    brainf

--- Brainf_compiler/brainf.c
+++ Brainf_compiler/brainf.c
@@ -1,67 +1,56 @@
 #include <stdio.h>
-int fun(int pos, char array[],char c)
-{
-        switch (c)
+
+void interpreter(FILE *source){
+    char instruction,data[100]={0};
+    int ip = 0, index=0;
+    while((instruction = fgetc(source)) !=EOF){
+        switch (instruction)
         {
-        case '>':
-            pos++;
-            break;
-        case '<':
-            pos--;
-            break;
         case '+':
-            array[pos]++;
+            data[ip]++;
             break;
         case '-':
-            array[pos]--;
+            data[ip]--;
+            break;
+        case '>':  \
+            ip++;
+            break;
+        case '<':
+            ip--;
             break;
         case '.':
-            printf("%c", array[pos]);
+            printf("%c", data[ip]);
             break;
         case ',':
-            scanf("%c", &array[pos]);
+            scanf("%c", &data[ip]);
+            break;
+        case '[':
+            if(data[ip] == 0){
+
+            }
+            break;
+        case ']':
+            if(data[ip] != 0){
+
+            }
             break;
         default:
+
             break;
         }
-}
-
-int looper(FILE *file, char array[], int pos)
-{
-    char c;
-    while ((c = fgetc(file)) != EOF)
-    {
-        if(c!='['){
-        fun(pos, array, c);
-        }
-        else if (c==']')
-        {
-            
-        }
-        
-        
     }
 }
 
-int main(int argc, char const *argv[])
+
+
+int main()
 {
-    // check program arguments
-    // if (argc != 2)
-    // {
-    //     printf("Usage: %s <file_name>\n", argv[0]);
-    //     return 1;
-    // }
-    // open file
-    FILE *file = fopen("./brainf.f", "r"); // argv[1]
-    if (file == NULL)
+    FILE *source = fopen("test.bf", "r");
+    if (source == NULL)
     {
         printf("Error opening file\n");
         return 1;
     }
-    int pos = 0;
-    char array[100] = {0},c;
-    
-    
-
+    interpreter(source);
     return 0;
 }
